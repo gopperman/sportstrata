822466d194a6e7678a0a4f114e8070ec98f59d0d
Greg Opperman <gopperman@gmail.com>
2016-10-22 21:46:18 -0400

    Team page header

--- template-team.php
+++ template-team.php
@@ -21,11 +21,15 @@ $teams = array(
   'directors' => $director_query->get_posts(),
   'coaches' => $coach_query->get_posts(),
   'trainers' => $trainer_query->get_posts(),
-); ?>
+);
+
+$header_description = get_post_meta( get_the_ID(), 'header_description')[0];
+
+?>
 <div id="content-header" class="hero">
   <div class="centered">
     <h1>Our Team</h1>
-    <h2>lorem ipsum</h2>
+    <h2><?php echo esc_html($header_description); ?></h2>
   </div>
 </div>
 <div class="team">
@@ -36,14 +40,16 @@ $teams = array(
           <h2><?php echo esc_html($team); ?></h2>
           <?php foreach ($members as $member) {
             $fields = get_post_meta( $member->ID ); ?>
-            <div class="team__member col-sm-6 col-md-4">
-                <a class="team__member-link" href="<?php echo get_permalink($member->ID); ?>">
-                  <img src="<?php echo esc_url( wp_get_attachment_image_src( $fields['external_content_headshot'][0], 'medium' )[0] ); ?>" />
-                </a>
-                <h3><a href="<?php echo get_permalink($member->ID); ?>">
-                  <?php echo esc_html($member->post_title); ?>
-                </a></h3>
-                <h4><?php echo esc_html($fields['external_content_title'][0]); ?></h4>
+            <div class="col-sm-6 col-md-4">
+              <div class="team__member">
+                  <a class="team__member-link" href="<?php echo get_permalink($member->ID); ?>">
+                    <img src="<?php echo esc_url( wp_get_attachment_image_src( $fields['external_content_headshot'][0], 'medium' )[0] ); ?>" />
+                  </a>
+                  <h3><a href="<?php echo get_permalink($member->ID); ?>">
+                    <?php echo esc_html($member->post_title); ?>
+                  </a></h3>
+                  <h4><?php echo esc_html($fields['external_content_title'][0]); ?></h4>
+              </div>
             </div>
           <?php } ?>
         </div>
