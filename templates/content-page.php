ea856774be96caaba38fd2fcbcc7ee601fcffcfb
Greg Opperman <gopperman@gmail.com>
2017-01-24 22:02:45 -0500

    Landing page templates and styles

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,4 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<div class="container-fluid">
+  <?php the_content(); ?>
+  <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+</div>
