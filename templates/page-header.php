9ecb1843fa61cde72538c56bbf4300e5e0db9ab1
Greg Opperman <gopperman@gmail.com>
2017-02-20 12:39:39 -0500

    Centered text on landing page. Closes #19

--- templates/page-header.php
+++ templates/page-header.php
@@ -4,9 +4,9 @@ $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'thumbnail-size', true
 $thumb_url = ( strpos( $thumb_url_array[0], 'default.png' ) ) ? '/app/themes/sportstrata/assets/images/sports2-lg.jpg' : $thumb_url_array[0];
 ?>
 
-<div class="page-header">
+<div class="page-header hero" id="content-header">
   <img src="<?= $thumb_url ?>" role="presentation" alt="" />
-  <div class="container-fluid">
-    <h2 class="page__title"><?= Titles\title(); ?></h2>
+  <div class="centered">
+      <h2 class="page__title"><?= Titles\title(); ?></h2>
   </div>
 </div>
